dbe2bf0f1fc544a03db70a88792f1c871c863302
Sukun444 <vanhein445566@gmail.com>
2024-08-22 20:57:18 +0700

    first commit

---
+++ src/main/java/vn/hust/hedspi/ezsport/EzsportApplication.java
@@ -0,0 +1,13 @@
+package vn.hust.hedspi.ezsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzsportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzsportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/hust/hedspi/ezsport/EzsportApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.hust.hedspi.ezsport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzsportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
