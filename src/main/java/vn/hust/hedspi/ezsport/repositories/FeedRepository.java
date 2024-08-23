0fab4a6fcb46a72c4e0a9ab0cda37da2ec53d7e8
Anh Duc Nguyen <kiaisoft.duc.nguyen@gmail.com>
2024-08-23 16:14:46 +0700

    feat: CRUD feed

---
+++ src/main/java/vn/hust/hedspi/ezsport/repositories/FeedRepository.java
@@ -0,0 +1,11 @@
+package vn.hust.hedspi.ezsport.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vn.hust.hedspi.ezsport.entities.Feed;
+
+import java.util.UUID;
+
+@Repository
+public interface FeedRepository extends JpaRepository<Feed, UUID> {
+}
