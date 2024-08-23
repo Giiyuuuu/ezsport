0e72e093d28a6fb7c2609d46b3e985fd2efa0777
Anh Duc Nguyen <kiaisoft.duc.nguyen@gmail.com>
2024-08-23 15:20:57 +0700

    feat: CRUD field order

---
+++ src/main/java/vn/hust/hedspi/ezsport/dtos/fieldOrder/UpdateFieldOrderRequest.java
@@ -0,0 +1,43 @@
+package vn.hust.hedspi.ezsport.dtos.fieldOrder;
+
+import jakarta.validation.constraints.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UpdateFieldOrderRequest {
+    @NotNull
+    UUID fieldId;
+
+    @NotNull
+    LocalTime start;
+
+    @NotNull
+    LocalTime end;
+
+    @NotNull
+    @FutureOrPresent
+    LocalDate date;
+
+    @NotNull
+    @DecimalMin("0.0")
+    double price;
+
+    @NotNull
+    UUID userId;
+
+    @NotNull
+    @Future
+    LocalDateTime paidAt;
+}
