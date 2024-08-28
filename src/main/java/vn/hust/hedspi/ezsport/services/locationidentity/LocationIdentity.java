65825d31f2ee31c082eb2d0c0da12a6ade30a57a
Sukun444 <vanhein445566@gmail.com>
2024-08-28 22:04:28 +0700

    test h3

---
+++ src/main/java/vn/hust/hedspi/ezsport/services/locationidentity/LocationIdentity.java
@@ -0,0 +1,7 @@
+package vn.hust.hedspi.ezsport.services.locationidentity;
+
+import java.io.IOException;
+
+public interface LocationIdentity {
+    public String getLocationIdentity(double longitude,double latitude) throws Exception;
+}
